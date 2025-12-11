0f4f397b967ef7d0b4096ad59d5c84fcba1259e7
RobotiMeIMir <154265963+RobotiMeIMir@users.noreply.github.com>
2025-12-11 17:35:23 +0100

    Update import cv2RGB.py

--- import cv2RGB.py
+++ import cv2RGB.py
@@ -1,5 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
 
 image = cv2.imread('example.jpg')
 
@@ -8,5 +7,6 @@ cv2.resizeWindow('Loaded Image , 800, 500')
 cv2.imshow('Loaded Image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
 print(f"Image Dimensions: {image.shape}")
